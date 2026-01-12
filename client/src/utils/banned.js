a5845a008ca600893596c6679979eec69c2d8c51
xicoldes <bigbyme@gmail.com>
2026-01-12 22:23:09 +0800

    feat: Add additional banned ID to the list

--- client/src/utils/banned.js
+++ client/src/utils/banned.js
@@ -4,6 +4,7 @@
 // You can find the ID in the URL (e.g., hianime.nz/anime/naruto-20 -> ID is 20)
 export const BANNED_IDS = [
     2981,
+    9057,
     6680, // Example // Example
     // Add your specific IDs here
 ];
