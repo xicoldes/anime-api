d2bda457b3b936559c1f667757d24e2755e5be4e
xicoldes <bigbyme@gmail.com>
2026-06-04 20:21:36 +0800

    Fix all website links for anime

--- client/src/utils/banned.js
+++ client/src/utils/banned.js
@@ -1,7 +1,7 @@
 // client/src/utils/banned.js
 
 // Add the MAL IDs of the anime/movies you want to hide here.
-// You can find the ID in the URL (e.g., hianime.nz/anime/naruto-20 -> ID is 20)
+// You can find the ID in the URL (e.g., miruro.tv/anime/naruto-20 -> ID is 20)
 export const BANNED_IDS = [
     2981,
     9057,
